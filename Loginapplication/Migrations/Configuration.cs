9d5cdd5b70b6e3c9efeed8057912bfaa20626645
abhishek rampi <abhishekabhi347@gmail.com>
2018-09-19 19:27:48 +0530

    mindata

--- Loginapplication/Migrations/Configuration.cs
+++ Loginapplication/Migrations/Configuration.cs
@@ -19,19 +19,44 @@ namespace Loginapplication.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+            
+            context.MenuManagement.AddOrUpdate(
+
+                M => M.Menu_NAME,
+                new MenuManagement { Menu_ID =1, Menu_Parent_ID = 0, Menu_NAME = "About", CONTROLLER_NAME = "Home", ACTION_NAME = "About" },
+                new MenuManagement { Menu_ID =2, Menu_Parent_ID = 0, Menu_NAME = "Contact", CONTROLLER_NAME = "Home", ACTION_NAME = "Contact" },
+                new MenuManagement { Menu_ID =3, Menu_Parent_ID = 0, Menu_NAME = "Technologies", CONTROLLER_NAME = "Technology", ACTION_NAME = "Index" },
+                new MenuManagement { Menu_ID =4, Menu_Parent_ID = 0, Menu_NAME = "Utilities", CONTROLLER_NAME = "", ACTION_NAME = "" },
+                new MenuManagement { Menu_ID =5, Menu_Parent_ID = 4, Menu_NAME = "File Upload", CONTROLLER_NAME = "Utility", ACTION_NAME = "Create" },
+                new MenuManagement { Menu_ID =6, Menu_Parent_ID = 4, Menu_NAME = "Role Management", CONTROLLER_NAME = "Utility", ACTION_NAME = "Roles" },
+                new MenuManagement { Menu_ID =7, Menu_Parent_ID = 4, Menu_NAME = "Users", CONTROLLER_NAME = "Utility", ACTION_NAME = "Users", Checkstatus = "N" },
+                new MenuManagement { Menu_ID =8, Menu_Parent_ID = 4, Menu_NAME = "Site Settings", CONTROLLER_NAME = "Utility", ACTION_NAME = "SiteSetting" }
+                );
+
+            context.siteSettings.AddOrUpdate(
+                M => M.SettingName,
+                new SiteSettings {SettingsID=1, SettingName = "Default", ApplicationTitle = "Sample Application", ApplicationTitleColour = "FFFFFF", ApplicationTitleFont = "Arial Verdana", ApplicationTitleSize = "20", MenuColour = "0BA5FF", MenuTextColour = "FFFFFF", NavColour = "1DA4FF", NavTextColour = "FFFFFF", IsActive = true }
+
+                );
+
+            context.Users.AddOrUpdate(
+               U=>U.EmpName,
+               new User { EmpName="Administrator",UserName="admin",Password= "2A-67-C0-9B-5E-50-79-63-AE-08-92-C1-23-54-96-52", Email="admin@gmail.com",Roleid=1,Vcode= "zzcdRo9XRX" }
+                
+                );
 
             context.Employees.AddOrUpdate(
                 E => E.Name,
-                new Employee { Name = "Abhishek", Company = "ISSI", Description = "Dev", IsEmployeeRetired = false, IsActive = true },
-                new Employee { Name = "Raju", Company = "Google", Description = "UI", IsEmployeeRetired = false, IsActive = true },
-                new Employee { Name = "Gautham", Company = "TCS", Description = "Support", IsEmployeeRetired = true, IsActive = true }
+                new Employee {EmployeeId=1,  Name = "Abhishek", Company = "ISSI", Description = "Dev", IsEmployeeRetired = false, IsActive = true },
+                new Employee {EmployeeId=2, Name = "Raju", Company = "Google", Description = "UI", IsEmployeeRetired = false, IsActive = true },
+                new Employee {EmployeeId=3, Name = "Gautham", Company = "TCS", Description = "Support", IsEmployeeRetired = true, IsActive = true }
                 );
 
             context.Roles.AddOrUpdate(
                 R => R.RoleName,
-                new Role { RoleName = "Admin", Menu_display = "1,2,3,4,5,6,8" },
-                new Role { RoleName = "Employee", Menu_display = "1,2,3" },
-                new Role { RoleName = "User", Menu_display = "2" }
+                new Role { Roleid=1, RoleName = "Admin", Menu_display = "1,2,3,4,5,6,8" },
+                new Role { Roleid=2, RoleName = "Employee", Menu_display = "1,2,3" },
+                new Role { Roleid=3, RoleName = "User", Menu_display = "2" }
                 );
 
             context.Countries.AddOrUpdate(
@@ -54,25 +79,6 @@ namespace Loginapplication.Migrations
                 new States { StateName = "Maryland", CountryId = 1 }
                 );
 
-
-            context.MenuManagement.AddOrUpdate(
-
-                M => M.Menu_NAME,
-                new MenuManagement { Menu_ID = 1, Menu_Parent_ID = 0, Menu_NAME = "About", CONTROLLER_NAME = "Home", ACTION_NAME = "About" },
-                new MenuManagement { Menu_ID = 2, Menu_Parent_ID = 0, Menu_NAME = "Contact", CONTROLLER_NAME = "Home", ACTION_NAME = "Contact" },
-                new MenuManagement { Menu_ID = 3, Menu_Parent_ID = 0, Menu_NAME = "Technologies", CONTROLLER_NAME = "Technology", ACTION_NAME = "Index" },
-                new MenuManagement { Menu_ID = 4, Menu_Parent_ID = 0, Menu_NAME = "Utilities", CONTROLLER_NAME = "", ACTION_NAME = "" },
-                new MenuManagement { Menu_ID = 5, Menu_Parent_ID = 4, Menu_NAME = "File Upload", CONTROLLER_NAME = "Utility", ACTION_NAME = "Create" },
-                new MenuManagement { Menu_ID = 6, Menu_Parent_ID = 4, Menu_NAME = "Role Management", CONTROLLER_NAME = "Utility", ACTION_NAME = "Roles" },
-                new MenuManagement { Menu_ID = 7, Menu_Parent_ID = 4, Menu_NAME = "Users", CONTROLLER_NAME = "Utility", ACTION_NAME = "Users", Checkstatus = "N" },
-                new MenuManagement { Menu_ID = 8, Menu_Parent_ID = 4, Menu_NAME = "Site Settings", CONTROLLER_NAME = "Utility", ACTION_NAME = "SiteSetting" }
-                );
-
-            context.siteSettings.AddOrUpdate(
-                M => M.SettingName,
-                new SiteSettings { SettingName = "Default", ApplicationTitle = "Sample Application", ApplicationTitleColour = "FFFFFF", ApplicationTitleFont = "Arial Verdana", ApplicationTitleSize = "20", MenuColour = "0BA5FF", MenuTextColour = "FFFFFF", NavColour = "1DA4FF", NavTextColour = "FFFFFF",IsActive= true }
-
-                );
 
 
             context.SaveChanges();
